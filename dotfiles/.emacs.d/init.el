888cd3fc3b209d5c61532d8baa9f1d87f8cf8e86
William Madruga <willmadruga@gmail.com>
2020-01-02 17:01:51 -0800

    more emacs config

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,18 +1,37 @@
+;; Proxy configuration
+(setq custom-file "~/.emacs.d/proxy.el")
+(load custom-file)
 
 ;; Package Setup ===============================================================
+
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
- 	     '("gnu" . "https://elpa.gnu.org/packages/"))
+
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 (package-initialize)
+(package-refresh-contents)
 
-;; Bootstrap 'use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
-;; For use-package options, see https://github.com/jwiegley/use-package
 
+(eval-when-compile (require 'use-package))
+
+;; load the rest of my init configuration:
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit which-key vimish-fold use-package try projectile origami org-bullets indent-guide golden-ratio flycheck dracula-theme counsel auto-package-update auto-complete aggressive-indent ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
